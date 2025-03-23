5c398f8263408078d5d5ae16d1db89770dc0bca8
Descriptik <descriptikk@gmail.com>
2025-03-23 19:44:11 +0100

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import Timer from './components/Timer';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-blue-100 to-purple-100 flex items-center justify-center p-4">
+      <Timer />
+    </div>
+  );
+}
+
+export default App;
